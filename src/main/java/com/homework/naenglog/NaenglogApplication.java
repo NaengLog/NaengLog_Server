283ed5ed9d6dc9fc8b055810c27bc06e382d2023
yongbin <rhdydqls1024@naver.com>
2022-10-25 19:54:18 +0900

    :seedling: :: 기본 세팅

---
+++ src/main/java/com/homework/naenglog/NaenglogApplication.java
@@ -0,0 +1,13 @@
+package com.homework.naenglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaenglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaenglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/naenglog/NaenglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.naenglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaenglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
